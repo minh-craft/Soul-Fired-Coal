f8298945812d797c2bb758d79457d3d73ab36220
minhty-fresh <4746495+mdaole@users.noreply.github.com>
2025-11-25 14:27:25 -0800

    initial template commit

---
+++ src/client/java/com/minhcraft/SoulfiredcoalClient.java
@@ -0,0 +1,10 @@
+package com.minhcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulfiredcoalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
